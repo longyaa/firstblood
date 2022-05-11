948cfa13151d73638e052e794bf0354921bdb622
longyaa <44659218+longyaa@users.noreply.github.com>
2022-05-11 11:01:11 +0800

    Update settings.py

--- mys10pro/settings.py
+++ mys10pro/settings.py
@@ -3,13 +3,12 @@ from redis import Redis
 from flask.sessions import SecureCookieSession
 class Config(object):
     DEBUG = True
-    SECRET_KEY = 'abcd'#session存cookie需要设置secret_key
+    SECRET_KEY = 'abcd' # session存cookie需要设置secret_key
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=20)
 
-    #PERMANENT_SESSION_LIFETIME这个东西是设置session超时的，它来自于flasksessions里面的SecureCookieSession
 
-    SESSION_REFRESH_EACH_REQUEST=True#设置session只要在20分钟内点击就不会失效
-    SESSION_TYPE = "redis"#配置session的保存类型
+    SESSION_REFRESH_EACH_REQUEST=True # 设置session只要在20分钟内点击就不会失效
+    SESSION_TYPE = "redis" # 配置session的保存类型
 
 class ProductionConfig(Config):
     SESSION_REDIS = Redis('127.0.0.1',port='6379')
